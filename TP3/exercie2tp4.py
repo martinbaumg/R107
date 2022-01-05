a82c2253c950dde46aefe9988192d4a58f016aaa
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-01-05 11:51:43 +0100

    v1.2

--- TP3/exercie2tp4.py
+++ TP3/exercie2tp4.py
@@ -3,9 +3,18 @@ moyenne = 0.0
 notes = []
 
 for i in range(nombreEtudiants):
-    while True:
-        try:
+    note = float(input(f"Note étudiant {i + 1}: "))
+    while 0 > note or note > 20:
             note = float(input(f"Note étudiant {i + 1}: "))
-            if 0 <= note <= 20:
-                notes.append(note)
-                moyenne += note
+
+    notes.append(note)
+    moyenne += note
+
+moyenne = moyenne/nombreEtudiants
+print(f"moyenne de la classe : {moyenne}")
+
+print(f"Numéro de l'étudiant | note | écart à la moyenne")
+
+for i in range(nombreEtudiants):
+
+    print(f"{i} | {notes[i]} | {moyenne-notes[i]}")
