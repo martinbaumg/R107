aac895a0c87e88941fec26243e26f3d20a0f9fce
martin baumgaertner <92078725+martinbaumg@users.noreply.github.com>
2022-01-06 10:00:15 +0100

    t

---
+++ TP3/exercie1tp5.py
@@ -0,0 +1,9 @@
+nomlist = []
+for i in range(0,2):
+    prenom = str(input("Prénom : "))
+    nom = str(input("Nom : "))
+    nomlist.append(" ".join([nom.upper(),prenom.capitalize()]))
+nomlist = sorted(nomlist, key=str.lower)
+for item in nomlist:
+    temp = item.split()
+    print(temp[1],temp[0])
